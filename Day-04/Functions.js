fcaed1b39c526c2d7a554fc6d37dc52219de32ef
Ryan <ryan.jeffwise@gmail.com>
2020-12-13 16:24:06 +1100

    Struggling with Part 2

--- Day-04/Functions.js
+++ Day-04/Functions.js
@@ -9,3 +9,41 @@ function parseFields(str) {
   return obj;
 }
 
+function dateValidation(date, minYear, maxYear) {
+  var isValid = true;
+  if (date.length != 4) {
+    isValid = false;
+  }
+  if (isValid) {
+    isValid = validateDateRange(Number(date), minYear, maxYear);
+  }
+  return isValid;
+}
+
+function validateDateRange(date, minYear, maxYear) {
+  var isValid = true;
+
+  if (date < minYear || date > maxYear) {
+    isValid = false;
+  }
+  return isValid;
+}
+
+function validateHGT(hgt, unit) {
+  var isValid = true;
+  switch (unit) {
+    case "cm":
+      if (hgt < 150 || hgt > 193) {
+        isValid = false;
+      }
+      break;
+    case "in":
+      if (hgt < 59 || hgt > 76) {
+        isValid = false;
+      }
+      break;
+    default:
+      isValid = false;
+  }
+  return isValid;
+}
